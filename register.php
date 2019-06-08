eca98ffc8003d88a2689abde129fec0a0557bb60
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-08 11:17:21 -0400

    Edited register.php to now accept input from forms...

--- register.php
+++ register.php
@@ -1,3 +1,61 @@
+<?php
+
+$con = mysqli_connect("localhost", "root", "", "social");
+if(mysqli_connect_errno())  {
+    echo"Failed to connect".mysqli_connect_errno();
+}
+
+//variables to prevent errors
+$fname = "";  //fiisrtname
+$lname ="";   //lastname
+$em = "";     //email
+$em2 = "";     //email2
+$password = ""; //password
+$password2 = ""; //password2
+$date = "";     //sign up date
+$error_array = "";    //error messages
+
+if(isset($_POST['register_button'])) {
+
+    //register form values
+
+
+ //first name...
+    $fname  = strip_tags($_POST['reg_fname']);    //strip tags function removes all html tags to prevent injections
+    $fname = str_replace(' ','',$fname);    //str replace function removes first argument and replaces them with second argument. 
+    $fname = ucfirst(strtolower($fname)); //lowers all letters in the string, and then capitalizes only the first letter
+
+ //last name...
+ $lname  = strip_tags($_POST['reg_lname']);    //strip tags function removes all html tags to prevent injections
+ $lname = str_replace(' ','',$lname);    //str replace function removes first argument and replaces them with second argument. 
+ $lname = ucfirst(strtolower($lname)); //lowers all letters in the string, and then capitalizes only the first letter
+
+
+ //Email...
+ $em  = strip_tags($_POST['reg_email']);    //strip tags function removes all html tags to prevent injections
+ $em = str_replace(' ','',$em);    //str replace function removes first argument and replaces them with second argument. 
+ $em = ucfirst(strtolower($em)); //lowers all letters in the string, and then capitalizes only the first letter
+
+
+ //Email2...
+ $em2  = strip_tags($_POST['reg_email2']);    //strip tags function removes all html tags to prevent injections
+ $em2 = str_replace(' ','',$em2);    //str replace function removes first argument and replaces them with second argument. 
+ $em2 = ucfirst(strtolower($em2)); //lowers all letters in the string, and then capitalizes only the first letter
+
+  //password...
+  $password  = strip_tags($_POST['reg_password']);    //strip tags function removes all html tags to prevent injections
+  $password2  = strip_tags($_POST['reg_password2']);    //strip tags function removes all html tags to prevent injections
+
+  $date = date("Y-m-d");  //sets signup date to proper date format
+ 
+
+
+}
+
+
+
+
+?>
 
 
 <!DOCTYPE html>
