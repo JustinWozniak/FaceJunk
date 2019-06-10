c8a74d2b48857eafa0c54762c1405ad4954b22e1
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-10 10:32:38 -0400

    Added RANDOM function to register.php. Pics a random image from 100 of my fractals to be used as the background

--- register.php
+++ register.php
@@ -5,10 +5,15 @@ require 'includes/form_handlers/login_handler.php';
 ?>
 
 
-<html>
+<html class="mainview">
 <head>
 	<title>Welcome to FaceJunk!</title>
 	<link rel="stylesheet" type="text/css" href="assets/css/register_style.css">
+	<script type="text/javascript">
+
+  var images = ['./assets/images/backgrounds/fractals/1.png', './assets/images/backgrounds/fractals/2.png','./assets/images/backgrounds/fractals/3.png','./assets/images/backgrounds/fractals/4.png','./assets/images/backgrounds/fractals/5.png','./assets/images/backgrounds/fractals/6.png','./assets/images/backgrounds/fractals/7.png','./assets/images/backgrounds/fractals/8.png','./assets/images/backgrounds/fractals/9.png','./assets/images/backgrounds/fractals/10.png','./assets/images/backgrounds/fractals/11.png','./assets/images/backgrounds/fractals/12.png','./assets/images/backgrounds/fractals/13.png','./assets/images/backgrounds/fractals/14.png','./assets/images/backgrounds/fractals/15.png','./assets/images/backgrounds/fractals/16.png','./assets/images/backgrounds/fractals/17.png','./assets/images/backgrounds/fractals/18.png','./assets/images/backgrounds/fractals/19.png','./assets/images/backgrounds/fractals/20.png'];
+  document.getElementsByClassName('mainview')[0].style.backgroundImage = 'url(' + images[Math.floor(Math.random() * images.length)] + ')';
+</script>
 </head>
 <body>
 
