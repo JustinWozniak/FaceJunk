b9a56136927aa57bd5f4eef589681eed4e62149a
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-10 22:26:18 -0400

    imported bootstrap files

--- includes/header.php
+++ includes/header.php
@@ -10,8 +10,21 @@ if(isset($_SESSION["username"]))    {
 
 ?>
 
-<html>
-<head>
-	<title>Welcome to FaceJunk!!!!~</title>
-</head>
-<body>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0, user-scalable=no"
+    />
+    <meta name="description" content="" />
+    <title>Welcome to FaceJunk!!!!~</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.js"></script>
+    <link rel="stylesheet" type="text/css" href="./assets/css/bootstrap.css"">
+  </head>
+  <body></body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php  
 include("includes/header.php");
-session_destroy();
+// session_destroy();
 ?>
 	<!DOCTYPE html>
 <html lang="en">
@@ -17,3 +17,13 @@ session_destroy();
 </html>
 
 gfdgfdgfdgfd
+<div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div>
