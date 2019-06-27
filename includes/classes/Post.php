9e5462d4a195d9abdd14f2bacd2b374b1745881f
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-06-27 12:01:48 -0400

    Didnt likenhow the comments box opened when you clicked a link. Added an if else to make it not open comments if name is clicked.post.php

--- includes/classes/Post.php
+++ includes/classes/Post.php
@@ -1,19 +1,22 @@
 <?php
-class Post {
+class Post
+{
 	private $user_obj;
 	private $con;
 
-	public function __construct($con, $user){
+	public function __construct($con, $user)
+	{
 		$this->con = $con;
 		$this->user_obj = new User($con, $user);
 	}
 
-	public function submitPost($body, $user_to) {
+	public function submitPost($body, $user_to)
+	{
 		$body = strip_tags($body); //removes html tags 
 		$body = mysqli_real_escape_string($this->con, $body);
 		$check_empty = preg_replace('/\s+/', '', $body); //Deltes all spaces 
-      
-		if($check_empty != "") {
+
+		if ($check_empty != "") {
 
 
 			//Current date and time
@@ -22,7 +25,7 @@ class Post {
 			$added_by = $this->user_obj->getUsername();
 
 			//If user is on own profile, user_to is 'none'
-			if($user_to == $added_by) {
+			if ($user_to == $added_by) {
 				$user_to = "none";
 			}
 
@@ -36,64 +39,62 @@ class Post {
 			$num_posts = $this->user_obj->getNumPosts();
 			$num_posts++;
 			$update_query = mysqli_query($this->con, "UPDATE users SET num_posts='$num_posts' WHERE username='$added_by'");
-
 		}
 	}
 
-	public function loadPostsFriends($data, $limit) {
+	public function loadPostsFriends($data, $limit)
+	{
 
-		$page = $data['page']; 
+		$page = $data['page'];
 		$userLoggedIn = $_SESSION['username'];
 
-		if($page == 1) 
+		if ($page == 1)
 			$start = 0;
-		else 
+		else
 			$start = ($page - 1) * $limit;
 
 
 		$str = ""; //String to return 
 		$data_query = mysqli_query($this->con, "SELECT * FROM posts WHERE deleted='no' ORDER BY id DESC");
 
-		if(mysqli_num_rows($data_query) > 0) {
+		if (mysqli_num_rows($data_query) > 0) {
 
 
 			$num_iterations = 0; //Number of results checked (not necasserily posted)
 			$count = 1;
 
-			while($row = mysqli_fetch_array($data_query)) {
+			while ($row = mysqli_fetch_array($data_query)) {
 				$id = $row['id'];
 				$body = $row['body'];
 				$added_by = $row['added_by'];
 				$date_time = $row['date_added'];
 
 				//Prepare user_to string so it can be included even if not posted to a user
-				if($row['user_to'] == "none") {
+				if ($row['user_to'] == "none") {
 					$user_to = "";
-				}
-				else {
+				} else {
 					$user_to_obj = new User($con, $row['user_to']);
 					$user_to_name = $user_to_obj->getFirstAndLastName();
-					$user_to = "to <a href='" . $row['user_to'] ."'>" . $user_to_name . "</a>";
+					$user_to = "to <a href='" . $row['user_to'] . "'>" . $user_to_name . "</a>";
 				}
 
 				//Check if user who posted, has their account closed
 				$added_by_obj = new User($this->con, $added_by);
-				if($added_by_obj->isClosed()) {
+				if ($added_by_obj->isClosed()) {
 					continue;
 				}
 
 				$user_logged_obj = new User($this->con, $userLoggedIn);
-				if($user_logged_obj->isFriend($added_by)){
+				if ($user_logged_obj->isFriend($added_by)) {
 
-					if($num_iterations++ < $start)
-						continue; 
+					if ($num_iterations++ < $start)
+						continue;
 
 
 					//Once 10 posts have been loaded, break
-					if($count > $limit) {
+					if ($count > $limit) {
 						break;
-					}
-					else {
+					} else {
 						$count++;
 					}
 
@@ -104,93 +105,78 @@ class Post {
 					$profile_pic = $user_row['profile_pic'];
 
 					?>
-					<script>
-	function toggle<?php echo $id; ?>() {
-	
-			let element = document.getElementById("toggleComment<?php echo $id; ?>");
+				<script>
+					function toggle<?php echo $id; ?>() {
+						let target = $(event.target);
+						if (!target.is("a")) {
+							let element = document.getElementById("toggleComment<?php echo $id; ?>");
 
-			if (element.style.display == "block")
-				element.style.display = "none";
-			else
-				element.style.display = "block";
-		
-		}
-
-
-</script>
-
-<?php
-	
-				
-	$comments_check = mysqli_query($this->con, "SELECT * FROM comments WHERE post_id='$id'");
-	$comments_check_num = mysqli_num_rows($comments_check);
-	
-	
-						//Timeframe
-					$date_time_now = date("Y-m-d H:i:s");
-					$start_date = new DateTime($date_time); //Time of post
-					$end_date = new DateTime($date_time_now); //Current time
-					$interval = $start_date->diff($end_date); //Difference between dates 
-					if($interval->y >= 1) {
-						if($interval == 1)
-							$time_message = $interval->y . " year ago"; //1 year ago
-						else 
-							$time_message = $interval->y . " years ago"; //1+ year ago
-					}
-					else if ($interval-> m >= 1) {
-						if($interval->d == 0) {
-							$days = " ago";
-						}
-						else if($interval->d == 1) {
-							$days = $interval->d . " day ago";
-						}
-						else {
-							$days = $interval->d . " days ago";
-						}
-
-
-						if($interval->m == 1) {
-							$time_message = $interval->m . " month". $days;
-						}
-						else {
-							$time_message = $interval->m . " months". $days;
-						}
-
-					}
-					else if($interval->d >= 1) {
-						if($interval->d == 1) {
-							$time_message = "Yesterday";
-						}
-						else {
-							$time_message = $interval->d . " days ago";
+							if (element.style.display == "block")
+								element.style.display = "none";
+							else
+								element.style.display = "block";
 						}
 					}
-					else if($interval->h >= 1) {
-						if($interval->h == 1) {
-							$time_message = $interval->h . " hour ago";
-						}
-						else {
-							$time_message = $interval->h . " hours ago";
-						}
-					}
-					else if($interval->i >= 1) {
-						if($interval->i == 1) {
-							$time_message = $interval->i . " minute ago";
-						}
-						else {
-							$time_message = $interval->i . " minutes ago";
-						}
-					}
-					else {
-						if($interval->s < 30) {
-							$time_message = "Just now";
-						}
-						else {
-							$time_message = $interval->s . " seconds ago";
-						}
+				</script>
+
+				<?php
+
+				$comments_check = mysqli_query($this->con, "SELECT * FROM comments WHERE post_id='$id'");
+				$comments_check_num = mysqli_num_rows($comments_check);
+
+
+				//Timeframe
+				$date_time_now = date("Y-m-d H:i:s");
+				$start_date = new DateTime($date_time); //Time of post
+				$end_date = new DateTime($date_time_now); //Current time
+				$interval = $start_date->diff($end_date); //Difference between dates 
+				if ($interval->y >= 1) {
+					if ($interval == 1)
+						$time_message = $interval->y . " year ago"; //1 year ago
+					else
+						$time_message = $interval->y . " years ago"; //1+ year ago
+				} else if ($interval->m >= 1) {
+					if ($interval->d == 0) {
+						$days = " ago";
+					} else if ($interval->d == 1) {
+						$days = $interval->d . " day ago";
+					} else {
+						$days = $interval->d . " days ago";
 					}
 
-					$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
+
+					if ($interval->m == 1) {
+						$time_message = $interval->m . " month" . $days;
+					} else {
+						$time_message = $interval->m . " months" . $days;
+					}
+				} else if ($interval->d >= 1) {
+					if ($interval->d == 1) {
+						$time_message = "Yesterday";
+					} else {
+						$time_message = $interval->d . " days ago";
+					}
+				} else if ($interval->h >= 1) {
+					if ($interval->h == 1) {
+						$time_message = $interval->h . " hour ago";
+					} else {
+						$time_message = $interval->h . " hours ago";
+					}
+				} else if ($interval->i >= 1) {
+					if ($interval->i == 1) {
+						$time_message = $interval->i . " minute ago";
+					} else {
+						$time_message = $interval->i . " minutes ago";
+					}
+				} else {
+					if ($interval->s < 30) {
+						$time_message = "Just now";
+					} else {
+						$time_message = $interval->s . " seconds ago";
+					}
+				}
+
+				$str .= "<div class='status_post' onClick='javascript:toggle$id()'>
 								<div class='post_profile_pic'>
 									<img src='$profile_pic' width='50'>
 								</div>
@@ -213,25 +199,18 @@ class Post {
 							<iframe src='comment_frame.php?post_id=$id' id='comment_iframe' frameborder='0'></iframe>
 							</div>
 							<hr>";
-				}
+			}
+		} //End while loop
 
-			} //End while loop
-
-			if($count > $limit) 
-				$str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
+		if ($count > $limit)
+			$str .= "<input type='hidden' class='nextPage' value='" . ($page + 1) . "'>
 							<input type='hidden' class='noMorePosts' value='false'>";
-			else 
-				$str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align: centre;'> No more posts to show! </p>";
-		}
-
-		echo $str;
-
-
+		else
+			$str .= "<input type='hidden' class='noMorePosts' value='true'><p style='text-align: centre;'> No more posts to show! </p>";
 	}
 
-
-
-
+	echo $str;
+}
 }
 
 ?>
