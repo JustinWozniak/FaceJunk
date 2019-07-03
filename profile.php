14881aedee1cfa6e8849abd177c4ae928bd3c960
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-02 22:42:43 -0400

    GUESS WE NEED A PROFILE PAGE! Started styling the profile page. added wallpaper.js, displaying profile image number of Posts, Likes and Friends

--- profile.php
+++ profile.php
@@ -1,10 +1,35 @@
-<?php 
+<?php
 include("includes/header.php");
- ?>
+if (isset($_GET['profile_username'])) {
+	$username = $_GET['profile_username'];
+	$user_details_query = mysqli_query($con, "SELECT * FROM users WHERE username='$username'");
+	$user_array = mysqli_fetch_array($user_details_query);
+	//num_friends is -1 due to fact that the array in the database starts with a comma
+	$num_friends = (substr_count($user_array['friend_array'], ",")) - 1;
+}
+
+
+?>
+
+<head>
 	
+</head>
+
+<body class="mainview">
+<script src="./assets/js/wallpaper.js"></script>
+	<div class="profile_left">
+		<img src="<?php echo $user_array['profile_pic']; ?>">
+		<div class="profile_info">
+			<p><?php echo "Posts: " . $user_array['num_posts']; ?></p>
+			<p><?php echo "Likes: " . $user_array['num_likes']; ?></p>
+			<p><?php echo "Friends: " . $num_friends ?></p>
+		</div>
+
+	</div>
+	</div>
 
 	<div class="main_column column">
-		This is a profile page!
+		<?php echo $username; ?>
 
 
 	</div>
@@ -14,4 +39,5 @@ include("includes/header.php");
 
 	</div>
 </body>
+
 </html>
