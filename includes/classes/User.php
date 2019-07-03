21ea1674ed951c92eb7e9dd79120d0955090af2d
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-03 09:39:18 -0400

    Styles buttons

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -68,6 +68,19 @@ class User {
 	}
 
 
+	public function didSendRequest($user_to) {
+		$user_from = $this->user['username'];
+		$check_request_query = mysqli_query($this->con, "SELECT * FROM friend_requests WHERE user_to='$user_to' AND user_from='$user_from'");
+		if(mysqli_num_rows($check_request_query) > 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+
+
 }
 
 ?>
