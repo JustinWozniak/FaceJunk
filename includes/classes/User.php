3a4436cff8acef59804f962e6e38ba2a6041e314
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-05 08:36:08 -0400

    finished friend request page

--- includes/classes/User.php
+++ includes/classes/User.php
@@ -67,6 +67,13 @@ class User {
 		}
 	}
 
+	public function getFriendArray() {
+		$username = $this->user['username'];
+		$query = mysqli_query($this->con, "SELECT friend_array FROM users WHERE username='$username'");
+		$row = mysqli_fetch_array($query);
+		return $row['friend_array'];
+	}
+
 
 	public function didSendRequest($user_to) {
 		$user_from = $this->user['username'];
--- request.php
+++ request.php
@@ -1,6 +1,5 @@
 <?php
-include("includes/header.php");
-
+include("includes/header.php"); //Header 
 ?>
 
 <div class="main_column column" id="main_column">
