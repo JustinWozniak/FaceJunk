75d8a3ecfe14071c6a83c62e878e15b8a23919f7
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-05 08:54:11 -0400

    Styled friend request buttons

--- requests.php
+++ requests.php
@@ -15,14 +15,14 @@ include("includes/header.php"); //Header
 
     $query = mysqli_query($con, "SELECT * FROM friend_requests WHERE user_to='$userLoggedIn'");
     if (mysqli_num_rows($query) == 0)
-        echo "You have no friend requests at this time!";
+        echo "Nobody wants to be your friend at this time ;(";
     else {
 
         while ($row = mysqli_fetch_array($query)) {
             $user_from = $row['user_from'];
             $user_from_obj = new User($con, $user_from);
 
-            echo $user_from_obj->getFirstAndLastName() . " sent you a friend request!";
+            echo "NO WAY! " .  $user_from_obj->getFirstAndLastName() . " sent you a friend request!";
 
             $user_from_friend_array = $user_from_obj->getFriendArray();
 
