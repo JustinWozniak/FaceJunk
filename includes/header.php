1a00e9bfe30ea6518bd9b9063ff42b321f175983
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-15 09:46:25 -0400

    I think infint scrolling work on messages now.....

--- includes/header.php
+++ includes/header.php
@@ -73,49 +73,49 @@ if (isset($_SESSION['username'])) {
 
 	$(document).ready(function() {
 
-		$(window).scroll(function() {
-			let inner_height = $('.dropdow_data_window').innerHeight(); //Div containing data
-			let scroll_top = $(.dropdow_data_window).scrollTop();
-			let page = $('.dropdow_data_window').find('.nextPageDropDownData').val();
-			let noMoreData = $('.dropdow_data_window').find('.noMoreDropdownData').val();
+$('.dropdown_data_window').scroll(function() {
+  let inner_height = $('.dropdown_data_window').innerHeight(); //Div containing data
+  let scroll_top = $('.dropdown_data_window').scrollTop();
+  let page = $('.dropdown_data_window').find('.nextPageDropdownData').val();
+  let noMoreData = $('.dropdown_data_window').find('.noMoreDropdownData').val();
 
-			if ((scroll_top + inner_height >= $('.dropdow_data_window')[0].scrollHeight && noMoreData == 'false') {
-        let pageName;//holds name of page to send request to
-        let type = $('dropdown_data_type').val();
-        if(type == 'notification')
-         pageName = "ajax_load_notifications.php";
-         else if (type == 'message')
-         pageName = "ajax_load_messages.php";
+  if ((scroll_top + inner_height >= $('.dropdown_data_window')[0].scrollHeight) && noMoreData == 'false') {
 
-				let ajaxReq = $.ajax({
-					url: "includes/handlers/ajax_load_posts.php",
-					type: "POST",
-					data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
-					cache:false,
-
-					success: function(response) {
-						$('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
-						$('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
-
-						$('#loading').hide();
-						$('.posts_area').append(response);
-					}
-				});
-
-			} //End if 
-
-			return false;
-
-		}); //End (window).scroll(function())
+    let pageName; //Holds name of page to send ajax request to
+    let type = $('#dropdown_data_type').val();
 
 
-	});
+    if(type == 'notification')
+      pageName = "ajax_load_notifications.php";
+    else if(type = 'message')
+      pageName = "ajax_load_messages.php"
 
-	</script>
-  </div>
 
-  <div class="wrapper">
+      let ajaxReq = $.ajax({
+      url: "includes/handlers/" + pageName,
+      type: "POST",
+      data: "page=" + page + "&userLoggedIn=" + userLoggedIn,
+      cache:false,
 
-    </body>
+      success: function(response) {
+        $('.dropdown_data_window').find('.nextPageDropdownData').remove(); //Removes current .nextpage 
+        $('.dropdown_data_window').find('.noMoreDropdownData').remove(); //Removes current .nextpage 
 
-</html>
+
+        $('.dropdown_data_window').append(response);
+      }
+    });
+
+  } //End if 
+
+  return false;
+
+}); //End (window).scroll(function())
+
+
+});
+
+</script>
+
+
+<div class="wrapper">
