739db284672a89f8767d8b4223b2040516eb35b1
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-22 00:51:07 -0400

    wrote the close account page

--- user_closed.php
+++ user_closed.php
@@ -4,7 +4,7 @@ include("includes/header.php");
 
 <div class="main_column column" id="main_column">
 	<h4>User Closed</h4>
-    LOSER!
+
 	This account is closed.
 	<a href="index.php"> Click here to go back.</a>
 
