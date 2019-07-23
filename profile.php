ad5f1e030876513531e594982502af15aa72cd93
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-23 10:24:11 -0400

    Finally fixed the newsfeed Bug. Not sure what it was...re-wrote the entire page from scratch. Also did more styling

--- profile.php
+++ profile.php
@@ -106,153 +106,150 @@ if(isset($_POST['post_message'])) {
 
   </div>
 
-  <div class="profile_main_column column">
+	<div class="profile_main_column column">
+
     <ul class="nav nav-tabs" role="tablist" id="profileTabs">
-      <li class="nav-item">
-        <a class="nav-link" href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab">Newsfeed</a>
-      </li>
-      <!-- <li class="nav-item">
-        <a class="nav-link" href="#about_div" aria-controls="about_div" role="tab" data-toggle="tab">About</a>
-      </li> -->
-      <li class="nav-item">
-        <a class="nav-link active" href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a>
-      </li>
+      <li role="presentation" class="active"><a href="#newsfeed_div" aria-controls="newsfeed_div" role="tab" data-toggle="tab" class='tabClass'>Newsfeed</a></li>
+      <li role="presentation"><a href="#messages_div" aria-controls="messages_div" role="tab" data-toggle="tab">Messages</a></li>
     </ul>
 
     <div class="tab-content">
 
-      <div role="tabpanel" class="tab-pane fade in active" id="newsfeed_div">
+      <div role="tabpanel" class="tab-pane active" id="newsfeed_div">
         <div class="posts_area"></div>
-        <img id="loading" src="assets/images/icons/pizzaLoad.gif">
+        <img id="loading" src="assets/images/icons/loading.gif">
       </div>
 
-      <div role="tabpanel" class="tab-pane fade" id="about_div">
-      </div>
 
-      <div role="tabpanel" class="tab-pane fade" id="messages_div">
-      <?php  
-        $message_obj = new Message($con, $userLoggedIn);
+      <div role="tabpanel" class="tab-pane" id="messages_div">
+        <?php  
+        
 
-        echo "<h4>You and <a href='" . $username ."'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
+          echo "<h4>You and <a href='" . $username ."'>" . $profile_user_obj->getFirstAndLastName() . "</a></h4><hr><br>";
 
-        echo "<div class='loaded_messages' id='scroll_messages'>";
-          echo $message_obj->getMessages($username);
-        echo "</div>";
-      ?>
+          echo "<div class='loaded_messages' id='scroll_messages'>";
+            echo $message_obj->getMessages($username);
+          echo "</div>";
+        ?>
 
 
-      <div class="message_post">
-        <form action="" method="POST">
-          <textarea name='message_body' id='message_textarea' placeholder='Write your message ...'></textarea>
-          <input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
-        </form>
 
-      </div>
-
-      <script>
-        var div = document.getElementById("scroll_messages");
-        div.scrollTop = div.scrollHeight;
-      </script>
-    </div>
-
-
-  </div>
-
-
-  </div>
-
-  <!-- Modal -->
-  <div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <div class="modal-content">
-
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-          <h4 class="modal-title" id="postModalLabel">Post something!</h4>
-        </div>
-
-        <div class="modal-body">
-          <p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
-
-          <form class="profile_post" action="" method="POST">
-            <div class="form-group">
-              <textarea class="form-control" name="post_body"></textarea>
-              <input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
-              <input type="hidden" name="user_to" value="<?php echo $username; ?>">
-            </div>
+        <div class="message_post">
+          <form action="" method="POST">
+              <textarea name='message_body' id='message_textarea' placeholder='Write your message ...'></textarea>
+              <input type='submit' name='post_message' class='info' id='message_submit' value='Send'>
           </form>
+
         </div>
 
+        <script>
+          $('a[data-toggle="tab"]').on('shown.bs.tab', function () {
+              var div = document.getElementById("scroll_messages");
+              div.scrollTop = div.scrollHeight;
+          });
+        </script>
+      </div>
 
-        <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Post</button>
-        </div>
+
+    </div>
+
+
+	</div>
+
+<!-- Modal -->
+<div class="modal fade" id="post_form" tabindex="-1" role="dialog" aria-labelledby="postModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+        <h4 class="modal-title" id="postModalLabel">Post something!</h4>
+      </div>
+
+      <div class="modal-body">
+      	<p>This will appear on the user's profile page and also their newsfeed for your friends to see!</p>
+
+      	<form class="profile_post" action="" method="POST">
+      		<div class="form-group">
+      			<textarea class="form-control" name="post_body"></textarea>
+      			<input type="hidden" name="user_from" value="<?php echo $userLoggedIn; ?>">
+      			<input type="hidden" name="user_to" value="<?php echo $username; ?>">
+      		</div>
+      	</form>
+      </div>
+
+
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary" name="post_button" id="submit_profile_post">Post</button>
       </div>
     </div>
   </div>
+</div>
 
 
-  <script>
-    var userLoggedIn = '<?php echo $userLoggedIn; ?>';
-    var profileUsername = '<?php echo $username; ?>';
+<script>
+  var userLoggedIn = '<?php echo $userLoggedIn; ?>';
+  var profileUsername = '<?php echo $username; ?>';
 
-    $(document).ready(function() {
+  $(document).ready(function() {
 
-      $('#loading').show();
+    $('#loading').show();
 
-      //Original ajax request for loading first posts 
-      $.ajax({
-        url: "includes/handlers/ajax_load_profile_posts.php",
-        type: "POST",
-        data: "page=1&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
-        cache: false,
+    //Original ajax request for loading first posts 
+    $.ajax({
+      url: "includes/handlers/ajax_load_profile_posts.php",
+      type: "POST",
+      data: "page=1&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+      cache:false,
 
-        success: function(data) {
-          $('#loading').hide();
-          $('.posts_area').html(data);
-        }
-      });
-
-      $(window).scroll(function() {
-        var height = $('.posts_area').height(); //Div containing posts
-        var scroll_top = $(this).scrollTop();
-        var page = $('.posts_area').find('.nextPage').val();
-        var noMorePosts = $('.posts_area').find('.noMorePosts').val();
-
-        if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
-          $('#loading').show();
-
-          var ajaxReq = $.ajax({
-            url: "includes/handlers/ajax_load_profile_posts.php",
-            type: "POST",
-            data: "page=" + page + "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
-            cache: false,
-
-            success: function(response) {
-              $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
-              $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
-
-              $('#loading').hide();
-              $('.posts_area').append(response);
-            }
-          });
-
-        } //End if 
-
-        return false;
-
-      }); //End (window).scroll(function())
-
-
+      success: function(data) {
+        $('#loading').hide();
+        $('.posts_area').html(data);
+      }
     });
+
+    $(window).scroll(function() {
+      var height = $('.posts_area').height(); //Div containing posts
+      var scroll_top = $(this).scrollTop();
+      var page = $('.posts_area').find('.nextPage').val();
+      var noMorePosts = $('.posts_area').find('.noMorePosts').val();
+
+      if ((document.body.scrollHeight == document.body.scrollTop + window.innerHeight) && noMorePosts == 'false') {
+        $('#loading').show();
+
+        var ajaxReq = $.ajax({
+          url: "includes/handlers/ajax_load_profile_posts.php",
+          type: "POST",
+          data: "page=" + page + "&userLoggedIn=" + userLoggedIn + "&profileUsername=" + profileUsername,
+          cache:false,
+
+          success: function(response) {
+            $('.posts_area').find('.nextPage').remove(); //Removes current .nextpage 
+            $('.posts_area').find('.noMorePosts').remove(); //Removes current .nextpage 
+            $('.posts_area').find('.noMorePostsText').remove(); //Removes current .nextpage 
+
+            $('#loading').hide();
+            $('.posts_area').append(response);
+              
+          }
+        });
+
+      } //End if 
+
+      return false;
+
+    }); //End (window).scroll(function())
+
+
+  });
+
   </script>
 
 
 
 
 
-  </div>
+	</div>
 </body>
-
 </html>
