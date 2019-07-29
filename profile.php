38e30ecb34bc83bdaa527f9ba8502ca377976709
Justin Wozniak <justin.wozniak@triosstudent.com>
2019-07-29 00:16:07 -0400

    Styled main newsfeed for cell phones

--- profile.php
+++ profile.php
@@ -117,7 +117,7 @@ if(isset($_POST['post_message'])) {
 
       <div role="tabpanel" class="tab-pane active" id="newsfeed_div">
         <div class="posts_area"></div>
-        <img id="loading" src="assets/images/icons/loading.gif">
+        <img id="loading" src="assets/images/icons/pizzaload.gif">
       </div>
 
 
